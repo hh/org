bcfbb3fb6ce9bfb0e250e6670f603addca2d5ecf
Hippie Hacker <hh@ii.coop>
2019-11-06 14:12:27 +0000

    Update osc52e session

--- start_osc52_session.sh
+++ start_osc52_session.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -x
 export ALTERNATE_EDITOR=""
-BASE=$(basename $1)
+export EMACSSOCKETNAME=$(basename $1)
+export II_SESSION=${USER}.${EMACSSOCKETNAME}
 OSC52E=$(dirname "${BASH_SOURCE[0]}")/osc52.sh
+xtermcontrol --title $USER.$EMACSSOCKETNAME@sharing.io
 # ensure this TMATE starts shell with BASE=$BASE
-tmate -S /tmp/${USER}.${BASE}.iisocket new-session \
+tmate -S /tmp/${II_SESSION}.iisocket new-session \
       -A -s $USER -n emacs \
       "tmate wait tmate-ready \
 && TMATE_CONNECT=\
@@ -13,4 +15,4 @@ tmate -S /tmp/${USER}.${BASE}.iisocket new-session \
 ; (echo \$TMATE_CONNECT | $OSC52E ) \
 ; echo Share the above with your friends and hit enter here when done? \
 ; read ; \
-emacsclient -s $BASE --tty $1 2>&1"
+emacsclient -s $EMACSSOCKETNAME --tty $1 2>&1"
