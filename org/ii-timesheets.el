8ab1df83fb3b12a3be57c64578aa94ae2e14f42b
Stephen Heywood <stephen@ii.coop>
2019-12-03 22:11:53 +1300

    Generate ii-timesheets via ISO week & year

---
+++ org/ii-timesheets.el
@@ -0,0 +1,39 @@
+(require 'cal-iso)
+
+(defun iso-week-to-time (year week day)
+  (pcase-let ((`(,m ,d ,y)
+               (calendar-gregorian-from-absolute
+                (calendar-iso-to-absolute (list week day year)))))
+    (encode-time 0 0 0 d m y)))
+
+(define-skeleton ii-timesheet
+  "Prompt the week and year before generating ii timesheet for the user."
+  ""
+  (text-mode)
+  > "#+TITLE: Timesheet: Week " (setq v1 (skeleton-read "Timesheet Week? "))
+  ", " (setq v2 (skeleton-read "Timesheet Year? "))
+  " (" (getenv "USER") ")" \n
+  > "#+AUTHOR: " (getenv "USER") \n
+  > " " \n
+  > "Please refer to the instructions in ii-timesheet.org as required." \n
+  > " " \n
+  > "* Week Summary" \n
+  > " " _ \n
+  > "#+BEGIN: clocktable :scope file :block thisweek :maxlevel 2 :emphasise t :tags t :formula %" \n
+  > "#+END" \n
+  > " " \n
+
+  > "* " (format-time-string "%B %e, %Y" (iso-week-to-time (string-to-number v2) (string-to-number v1) 1)) \n
+  > "** Task X" \n
+  > "* " (format-time-string "%B %e, %Y" (iso-week-to-time (string-to-number v2) (string-to-number v1) 2)) \n
+  > "** Task X" \n
+  > "* " (format-time-string "%B %e, %Y" (iso-week-to-time (string-to-number v2) (string-to-number v1) 3)) \n
+  > "** Task X" \n
+  > "* " (format-time-string "%B %e, %Y" (iso-week-to-time (string-to-number v2) (string-to-number v1) 4)) \n
+  > "** Task X" \n
+  > "* " (format-time-string "%B %e, %Y" (iso-week-to-time (string-to-number v2) (string-to-number v1) 5)) \n
+  > "** Task X" \n
+  > " " \n
+  (org-mode)
+  (save-buffer)
+)
