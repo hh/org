b17866bb4211215536ae063c902b1cce21da8ae1
Bernokl <bb@ii.coop>
2021-06-23 14:31:06 +1200

    Update retain script in asn-data-pipeline
    Co-Authored-By: Caleb Woodbine <caleb@ii.coop>

--- research/asn-data-pipeline/pg-init.d/10-stop-postgres.sh
+++ research/asn-data-pipeline/pg-init.d/10-stop-postgres.sh
@@ -2,13 +2,17 @@
 
 # Stop the database
 
-if [ "${ASN_DATA_PIPELINE_RETAIN}" = true ]; then
-    exit 0
+if [ ! "${ASN_DATA_PIPELINE_RETAIN}" = true ]; then
+  # in the Postgres container image,
+  # the command run changes to "postgres" once it's completed loading up
+  # and is in a ready state
+  #
+  # here we wait for that state and attempt to exit cleanly, without error
+  (
+    until [ "$(cat /proc/1/cmdline | tr '\0' '\n' | head -n 1)" = "postgres" ]; do
+      sleep 1s
+    done
+    # exit Postgres with a code of 0
+    pg_ctl kill QUIT 1
+  ) &
 fi
-
-(
-  until [ "$(cat /proc/1/cmdline | tr '\0' '\n' | head -n 1)" = "postgres" ]; do
-    sleep 1s
-  done
-  pg_ctl kill QUIT 1
-) &
