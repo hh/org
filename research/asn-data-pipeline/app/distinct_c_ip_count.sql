5ef1714a77770ff6ce2fcb51b1ff333d068516a1
Bernokl <bb@ii.coop>
2021-06-25 17:20:26 +1200

    Pipeline working up to asn network range lookup

--- research/asn-data-pipeline/app/distinct_c_ip_count.sql
+++ research/asn-data-pipeline/app/distinct_c_ip_count.sql
@@ -1,1 +1,1 @@
-SELECT DISTINCT c_ip, COUNT(c_ip) AS Total_Count FROM `${GCP_BIGQUERY_DATASET_BK}.usage_all_raw` GROUP BY c_ip ORDER BY Total_Count DESC
+SELECT DISTINCT c_ip, COUNT(c_ip) AS Total_Count FROM `${GCP_BIGQUERY_DATASET}.usage_all_raw` GROUP BY c_ip ORDER BY Total_Count DESC
