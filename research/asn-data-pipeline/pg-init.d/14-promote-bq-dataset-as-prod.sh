2cf59669c3a8767a644bbd550d3b1a7f4ed75fff
Caleb Woodbine <calebwoodbine.public@gmail.com>
2021-08-12 19:50:08 +1200

    Update dataset promotion

--- research/asn-data-pipeline/pg-init.d/14-promote-bq-dataset-as-prod.sh
+++ research/asn-data-pipeline/pg-init.d/14-promote-bq-dataset-as-prod.sh
@@ -1,4 +1,6 @@
-# bq rm -r -f "${GCP_BIGQUERY_DATASET}_$(date +%Y%m%d)"
-for TABLE in $(bq ls etl_script_generated_set_20210810 | awk '{print $1}' | tail +3 | xargs); do
-    bq cp -f "${GCP_BIGQUERY_DATASET}_20210810.$TABLE" "${GCP_BIGQUERY_DATASET}.$TABLE"
+for TABLE in $(bq ls ${GCP_BIGQUERY_DATASET}_$(date --date=yesterday +%Y%m%d) | awk '{print $1}' | tail +3 | xargs); do
+    echo "Removing table '${GCP_BIGQUERY_DATASET}.$TABLE'"
+    bq rm -f "${GCP_BIGQUERY_DATASET}.$TABLE";
+    echo "Copying table '${GCP_BIGQUERY_DATASET}_20210810.$TABLE' to '${GCP_BIGQUERY_DATASET}.$TABLE'"
+    bq cp --noappend_table --nono_clobber -f "${GCP_BIGQUERY_DATASET}_20210810.$TABLE" "${GCP_BIGQUERY_DATASET}.$TABLE";
 done
