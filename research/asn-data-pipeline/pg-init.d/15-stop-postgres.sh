a0d903b726ce154cc0a4d3ff28bb336652a7fa3b
Caleb Woodbine <calebwoodbine.public@gmail.com>
2021-08-26 17:24:54 +1200

    Update ASN data pipeline

--- research/asn-data-pipeline/pg-init.d/15-stop-postgres.sh
+++ research/asn-data-pipeline/pg-init.d/15-stop-postgres.sh
@@ -1,10 +1,11 @@
 if [ ! "${ASN_DATA_PIPELINE_RETAIN:-}" = true ]; then
     # in the Postgres container image,
     # the command run changes to "postgres" once it's completed loading up
-    # and is in a ready state
+    # and is in a ready state and all of the init scripts have run
     #
     # here we wait for that state and attempt to exit cleanly, without error
     (
+        # discover where the postgres process is, even if Prow has injected a PID 1 process
         PARENTPID=$(ps -o ppid= -p $$ | awk '{print $1}')
         echo MY PID     :: $$
         echo PARENT PID :: $PARENTPID
