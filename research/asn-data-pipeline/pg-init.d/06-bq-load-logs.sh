908c35094cbb6c8ef963485f44ad6a933956f689
Caleb Woodbine <calebwoodbine.public@gmail.com>
2021-09-07 17:01:02 +1200

    Fix date for promoting tables

--- research/asn-data-pipeline/pg-init.d/06-bq-load-logs.sh
+++ research/asn-data-pipeline/pg-init.d/06-bq-load-logs.sh
@@ -13,6 +13,6 @@ if [ -z "${GCP_BIGQUERY_DATASET_LOGS:-}" ]; then
         us.artifacts.k8s-artifacts-prod.appspot.com
     )
     for BUCKET in ${BUCKETS[*]}; do
-            bq load --autodetect --max_bad_records=2000 ${GCP_BIGQUERY_DATASET}_$(date +%Y%m%d).usage_all_raw gs://k8s-infra-artifacts-gcslogs/${BUCKET}_usage* || true
+            bq load --autodetect --max_bad_records=2000 ${GCP_BIGQUERY_DATASET}_$(date +%Y%m%d).usage_all_raw gs://k8s-artifacts-gcslogs/${BUCKET}_usage* || true
     done
 fi
